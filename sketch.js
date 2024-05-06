59fcee08fe8e81c71c39332481b4135148438c5d
floof <168108076+floofiekisses@users.noreply.github.com>
2024-05-06 10:48:55 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,32 @@
 let kisses;
 //create kisses sprite
-
-var font;
+let currentDialogue;
+let currentExpression;
+let invisibleExpression;
+let currentDialogueIndex = 0; // keeps track of which dialogue we're at
+let maxDialogue; // max dialogue amount
+let currentSprite;
+let displayTextBox = false;
 let startButton; // start button variable
+let font;
+let floofie = {};
+let player = {};
+
+let characters = {
+  floofie: floofie,
+  player: player,
+}
+// characters!!!!!
+
+let expressions = ["angry", "flustered", "happy", "normal", "sad", "sleepy"];
+// floofie expressions
 
 let title;
 let roomEntranceScene;
 let bedroomScene;
 // room variables
-let dialogueBox;
-// dialogue box variable
+let titleScreenTheme
 let floofieTalkingSound;
-let titleScreenTheme;
 // sound fx
 let bed;
 let carpet;
@@ -19,41 +34,89 @@ let moss;
 let planter;
 let sofaKissesBed;
 let table;
-//furniture
+//furniture sprites
+
 
 function preload() {
+for (let i = 0; i < expressions.length; i++) {
+  floofie[`${expressions[i]}`] = loadImage(`characters/floofie/expressions/${expressions[i]}.png`);
+}
+
+
+
   title = loadImage('titleScreen.png');
   roomEntranceScene = loadImage('floofieRoomEntrance.png');
   bedroomScene = loadImage('bedroom_furniture/floofieBedroomBG.png');
   font = loadFont('Minecraft.ttf');
-  font.addClass('font')
-  soundFormats('mp3');
   titleScreenTheme = loadSound('floofieTitleTheme.mp3');
-  titleScreenTheme.play;
+  floofieTalkingSound = loadSound("floofieTalking.mp3");
+  invisibleExpression = loadImage("characters/player/invisibleExpression.png");
 }
 
 function setup() {
   createCanvas(400, 240); // size of canvas
   background(title);
-  titleScreenTheme = createAudio('floofieTitleTheme')
   startButton = createButton('Start')
   startButton.addClass('startButton')
   startButton.position(125, 208); // sets button position
   startButton.mousePressed(entranceScene); // press mouse button to change scene
+  titleScreenTheme.play();
+  titleScreenTheme.setVolume(0.25);
+
+}
+
+function dialogue(sprite) {
+  // set dialogue
+  maxDialogue = sprite.dialogue.length;
+  // how long the interaction is
+  currentDialogue = sprite.dialogue[currentDialogueIndex];
+  //start @ 0
+  let currentCharacter = characters[sprite.characters];
+  //the character currently displayed
+  if (currentCharacter != "player") {
+    currentExpression = currentCharacter[sprite.expression[currentDialogueIndex]];
+  } else {
+    // if it's the player, set their expression to an invisible placeholder image.
+    currentExpression = invisibleExpression;
+  }
+  // this is the character's current expression according to the dialogue line current being displayed
+  displayTextBox = true;
+}
+
+
+function keyPressed() {
+  if (keyCode === RETURN && displayTextBox == true) {
+    if (currentDialogueIndex < maxDialogue - 1) {
+      currentDialogueIndex++;
+    } else {
+      currentDialogueIndex = 0;
+      displayTextBox = false;
+      //
+    }
+    console.log(currentDialogueIndex);
+
+    dialogue(currentSprite);
+    floofieTalkingSound.play(0.1);
+    floofieTalkingSound.duration()
+  }
 }
 
   // loading scenes and start button
 function entranceScene() {
   background(roomEntranceScene)
   if (startButton.mousePressed) {
+    titleScreenTheme.stop();
     startButton.hide();
-    dialogueBox = createButton();
-    dialogueBox.position(135, 175)
-    dialogueBox.addClass('dialogueBox');
-  }
+    currentDialogueIndex = 0;
+    displayTextBox = true;
+    currentSprite = introductionInteraction;
+    dialogue(currentSprite); // run introduction interaction dialogue
+    floofieTalkingSound.play(0.1);
+    }
 }
 
 // function bedroomScene() {
+// if (currentDialogueIndex === 0 && background.mousePressed = true) {
 //   background(bedroomScene);
 //   kisses = new Sprite(random(0, 400), random(0,240));
 //   kisses.img = 'kissesSprite.png';
@@ -71,20 +134,24 @@ function entranceScene() {
 //   table = newSprite();
 //   table.img = 'bedroom_furniture/table.png';
 // }
-
-// function runDialogue() {
-//   // do the dialog thingy :D
-//   if (keyCode === ENTER) {
-//   // whatever i need to do i have no idea yet LOL
-//   }
 // }
 
 
 // function kissesFound() {
-// if (kisses.mousePressed === true) {
+// if (kisses.mousePressed = true) {
 //   }
 // }
 
 function draw() {
+  // draw the textbox
+  // draw the text
+  if(displayTextBox) {
+    textFont(font);
+      textAlign(CENTER);
+      text(currentDialogue, 60, 200, width - 90, 100);
+    // show the current dialogue
+    image(currentExpression, 5, height - 80);
+      // draw the expression
+  }
 
 }
