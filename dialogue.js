20f8fc1a7f96850f0ada7b929b37c9fcfe18fde6
floof <168108076+floofiekisses@users.noreply.github.com>
2024-05-07 15:15:17 -0500

    Add files via upload

---
+++ dialogue.js
@@ -0,0 +1,53 @@
+let introductionInteraction = {
+    dialogue: ["Hi! Thanks for coming in here to help out.", "Recently, Kisses went missing and I", "haven't been able to find them anywhere!", "Could you please help me out?", "Yay! Thank you so much."],
+    characters: "floofie",
+    expression: ["happy", "sad", "sad", "normal", "happy"]
+}
+
+let tutorial = {
+    dialogue: ["Welcome to my humble abode! Hehe.",
+    "What you have to do now is",
+    "click your mouse to click on the furniture.",
+    "And hopefully. you'll find Kisses sooner than later.",
+    "I'm gonna head out to take care of my plants!",
+    "*Yawn*",
+    "Let me know when you find them.",
+    "Bye bye!"],
+    characters: "floofie",
+    expression: ["happy","normal", "normal", "normal", "happy", "sleepy", "normal"]
+}
+
+// floofie dialogue part 1
+let planterInteraction = {
+    dialogue: ["The lavenders smell really fresh and sweet…"],
+    characters: "player",
+    expression:"invisibleExpression"
+}
+let carpetInteraction = {
+    dialogue: ["The carpet is almost like a little cloud! It's so fluffy."],
+    characters: "player",
+    expression: "invisibleExpression"
+
+}
+let sofaKissesInteraction = {
+    dialogue: ["This must be where Floofie sits down and snuggles with Kisses.", "Kisses must be so nice to snuggle with!"],
+    characters: "player",
+    expression: ["invisibleExpression", "invisibleExpression"]
+}
+let tableInteraction = {
+    dialogue: ["Floofie's laptop is open… I probably shouldn't look through it."],
+    characters: "player",
+    expression: "invisibleExpression"
+}
+let playerWin = {
+    dialogue: ["Yippee! I found Kisses"],
+    characters: "player",
+    expression: "invisibleExpression",
+}
+    // player dialogue
+let findKisses = {
+    dialogue: ["Oh my goodness, you found them!", "Thank you so much, I was so worried.", "Now I can continue my journey!"],
+    characters: "floofie",
+    expression: ["happy", "sad", "happy"]
+}
+// find kisses dialogue
